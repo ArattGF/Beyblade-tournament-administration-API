3d4556fc5d3357adb535fe6a2619563876c5120c
ArattGF <arattgf@gmail.com>
2025-03-02 14:57:01 -0600

    Change project structure

--- app/index.js
+++ app/index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors');
+const AuthMiddleware = require('./middleware/AuthMiddleware')
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json()); 
 
 
 
 app.use('/tournament', require('../Tournament/Routes/TournamentRoutes'))
 app.use('/admin', require('../Admin/Routes/AdminRoutes'))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/',AuthMiddleware ,(req, res) => res.send('Hello World!'))
 
 
 mongoose.connect(process.env.MONGODB_URI).then  (() => {
