0e2ad8a06a36bd98b684d9a2647eb9a88722c541
ArattGF <arattgf@gmail.com>
2025-03-09 17:49:46 -0600

    Update CORS configuration to allow requests from specific origins

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: '*'//['http://localhost:4200', 'https://hidrobladers.vercel.app']
+  origin: ['http://localhost:4200', 'https://hidrobladers.vercel.app']
 }));
 app.use(express.json()); 
 
